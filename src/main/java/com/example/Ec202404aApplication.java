e777886d427f9ea3c8b03376e5546e2db6d8f9e9
yusaku-terashima <terassy0809@docomo.ne.jp>
2024-06-17 14:27:09 +0900

    first commit

---
+++ src/main/java/com/example/Ec202404aApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec202404aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec202404aApplication.class, args);
+	}
+
+}
