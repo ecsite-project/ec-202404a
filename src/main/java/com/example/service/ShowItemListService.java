51ca3c8da10f68ba00df9d11407ecdaf7f4781c3
krkrHotaru <meguda718@gmail.com>
2024-06-18 13:20:15 +0900

    商品一覧を表示させるためのメソッド作成

---
+++ src/main/java/com/example/service/ShowItemListService.java
@@ -0,0 +1,32 @@
+package com.example.service;
+
+import com.example.domain.Item;
+import com.example.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * 商品情報一覧を持ってくるサービス.
+ *
+ * @author krkrHotaru
+ */
+
+@Service
+@Transactional
+public class ShowItemListService {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    /**
+     * 全商品情報から10件持ってきます.
+     *
+     * @return 商品情報リスト
+     */
+    public List<Item> showItemList(){
+        return itemRepository.findAll();
+    }
+}
