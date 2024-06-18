4f39048132d13226bf345a7a68f5b465fdde43ef
krkrHotaru <meguda718@gmail.com>
2024-06-18 11:39:34 +0900

    レビュー内容の修正

--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -43,6 +43,7 @@ public class User {
                 ", municipalities='" + municipalities + '\'' +
                 ", address='" + address + '\'' +
                 ", telephone='" + telephone + '\'' +
+                ", adminFlag=" + adminFlag +
                 ", bookmarkList=" + bookmarkList +
                 '}';
     }
