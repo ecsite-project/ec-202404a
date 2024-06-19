2c6ba436df9d99b8a17358c7387231a46a98c64d
sumiship <osumi.k.osumi.k@gmail.com>
2024-06-19 13:39:58 +0900

    新規登録ページの住所取得JSを修正

--- src/main/resources/static/js/register/input-zipcode.js
+++ src/main/resources/static/js/register/input-zipcode.js
@@ -1,25 +1,31 @@
 'use strict'
 
-$(function () {
-  $('#searchAddressBtn').on('click', () => {
-    $.ajax({
-      url: 'https://zipcoda.net/api',
-      type: 'get',
-      dataType: 'json',
-      data: {
-        zipcode: $('#zipcode').val(),
-      },
-      async: true,
-    })
-      .done((data) => {
-        $('#prefecture').val(data.items[0].components[0])
-        $('#municipalities').val(data.items[0].components[1])
-        $('#address').val(data.items[0].components[2])
-      })
-      .fail((XMLHttpRequest, textStatus, errorThrown) => {
-        console.log(XMLHttpRequest)
-        console.log(textStatus)
-        console.log(errorThrown)
-      })
+$('#zipcode').on('keyup', () => {
+  const zipcode = $('#zipcode').val()
+  if (zipcode.replace('-', '').length < 7) return
+  $.ajax({
+    url: 'https://zipcoda.net/api',
+    type: 'get',
+    dataType: 'json',
+    data: {
+      zipcode,
+    },
+    async: true,
   })
+    .done((data) => {
+      $('#prefecture').val(data.items[0].components[0])
+      $('#prefecture').next().addClass('active')
+      $('#prefecture').addClass('valid')
+      $('#municipalities').val(data.items[0].components[1])
+      $('#municipalities').next().addClass('active')
+      $('#municipalities').addClass('valid')
+      $('#address').val(data.items[0].components[2])
+      $('#address').next().addClass('active')
+      $('#address').addClass('valid')
+    })
+    .fail((XMLHttpRequest, textStatus, errorThrown) => {
+      console.log(XMLHttpRequest)
+      console.log(textStatus)
+      console.log(errorThrown)
+    })
 })
