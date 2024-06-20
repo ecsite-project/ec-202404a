fa7b1b870d8db8a8c98e33ffc3e6d736bde25111
sumiship <osumi.k.osumi.k@gmail.com>
2024-06-20 18:07:44 +0900

    とりあえずコミット

---
+++ src/main/resources/static/js/materialize/date-picker.js
@@ -0,0 +1,18 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.datepicker')
+  const today = new Date()
+  const maxDate = new Date()
+  maxDate.setDate(today.getDate() + 7)
+
+  const options = {
+    autoClose: true,
+    format: 'yyyy/m/d',
+    defaultDate: today,
+    setDefaultDate: true,
+    minDate: today,
+    maxDate,
+  }
+  var instances = M.Datepicker.init(elems, options)
+})
