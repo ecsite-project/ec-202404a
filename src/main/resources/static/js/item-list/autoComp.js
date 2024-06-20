1ae675defadd727f09edc4fbfc944b5cbb76a6be
krkrHotaru <meguda718@gmail.com>
2024-06-20 11:04:17 +0900

    jsに不要なコンソール出力があったため修正

--- src/main/resources/static/js/item-list/autoComp.js
+++ src/main/resources/static/js/item-list/autoComp.js
@@ -12,7 +12,6 @@ $(function () {
     })
       .done(function (data) {
         let names = data.itemNameList
-        console.log(names)
         $('#searchWord ').autocomplete({
           source: names,
         })
