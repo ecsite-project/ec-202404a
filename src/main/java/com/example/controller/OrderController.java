3be6b0939eba7831957410355b23b61e7e1e49ce
Ruii1112 <ruiiiii1112@gmail.com>
2024-06-21 19:03:47 +0900

    メール送信ができないバグの改修

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -88,6 +89,11 @@ public class OrderController {
             e.printStackTrace();
         }
 
+        return "redirect:/order-complete";
+    }
+
+    @GetMapping("/order-complete")
+    public String orderComplete(){
         return "order-complete";
     }
 }
