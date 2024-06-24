8cc71ee55e4f27992d3ee03eb09354f81dc5ed94
Ruii1112 <ruiiiii1112@gmail.com>
2024-06-24 10:23:58 +0900

    エラーページの作成
    設定ファイルの編集
    
    エラーページの作成

--- src/main/java/com/example/controller/ShowItemDetailController.java
+++ src/main/java/com/example/controller/ShowItemDetailController.java
@@ -11,8 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Objects;
-
 /**
  * 商品の詳細表示機能を操作するコントローラ.
  *
