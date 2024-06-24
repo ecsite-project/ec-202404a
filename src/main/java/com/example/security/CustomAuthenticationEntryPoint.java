8443105ce417700946180cb1e230a8eeec641acc
igamasayuki <igamasayuki@gmail.com>
2024-06-25 08:29:46 +0900

    Update CustomAuthenticationEntryPoint.java
    JavaDocコメント追加

--- src/main/java/com/example/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/example/security/CustomAuthenticationEntryPoint.java
@@ -10,8 +10,25 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+/**
+ * カスタム認証エントリーポイントクラス.
+ *
+ * このクラスは、認証が必要なリソースに対して未認証のユーザーがアクセスした場合に呼び出されます。
+ * 未認証のユーザーをカスタムエラーページにリダイレクトする役割を果たします。
+ *
+ * @author igamasayuki
+ */
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    /**
+     * 認証が必要なリソースに未認証のユーザーがアクセスした場合に呼び出されます。
+     * 
+     * @param request HttpServletRequestオブジェクト
+     * @param response HttpServletResponseオブジェクト
+     * @param authException 発生した認証例外
+     * @throws IOException 入出力例外が発生した場合
+     * @throws ServletException サーブレット例外が発生した場合
+     */
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
 
